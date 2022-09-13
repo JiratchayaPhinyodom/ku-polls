5b1ce8a1a2fd6e3b1106dc85e73e3b35aa819813
jiratchayaphinyodom <jiratchaya.phiny@ku.th>
2022-09-13 19:50:17 +0700

    Add docstring models.py and views.py #29

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,4 @@
+"""Question models."""
 import datetime
 from django.contrib import admin
 from django.db import models
@@ -5,11 +6,16 @@ from django.utils import timezone
 
 
 class Question(models.Model):
+    """Question model class represent question_text pub_date and end_date."""
+
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    end_date = models.DateTimeField('the ending date for voting.', default=timezone.now, blank=True, null=True)
+    end_date = models.DateTimeField('the ending date for voting.',
+                                    default=timezone.now,
+                                    blank=True, null=True)
 
     def __str__(self):
+        """:return The question text."""
         return self.question_text
 
     @admin.display(
@@ -17,25 +23,41 @@ class Question(models.Model):
         ordering='pub_date',
         description='Published recently?',
     )
-
     def was_published_recently(self):
+        """
+        Check the question that was published recently.
+
+        :return boolean True if question was published recently.
+        """
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
     def is_published(self):
+        """
+        Check the question that is published.
+
+        :return boolean True if question is published.
+        """
         now = timezone.now()
         return now >= self.pub_date
 
     def can_vote(self):
+        """
+        Check the question at the time that can vote.
+
+        :return boolean True if the question is at the time of voting.
+        """
         now = timezone.now()
-        if self.end_date:
-            return (self.pub_date <= now) and (now <= self.end_date)
-        return self.is_published()
+        return self.pub_date <= now <= self.end_date
+
 
 class Choice(models.Model):
+    """Choice model class represent question choice_text and votes."""
+
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
+        """:return The choice text."""
         return self.choice_text
--- polls/views.py
+++ polls/views.py
@@ -1,17 +1,18 @@
+"""Views of polls app."""
 from django.utils import timezone
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse
 from django.views import generic
 from django.contrib import messages
-
 from .models import Choice, Question
 
 
 def get_queryset(self):
-    """
-    Return the last five published questions (not including those set to be
-    published in the future).
+    """Get queryset function.
+
+    :return the last five published questions.
+    (not including those set to be published in the future).
     """
     return Question.objects.filter(
         pub_date__lte=timezone.now()
@@ -19,15 +20,19 @@ def get_queryset(self):
 
 
 class IndexView(generic.ListView):
+    """Question index page that display the latest question."""
+
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """:return the last five published questions."""
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by(
+            '-pub_date')[:5]
 
 
 def detail(request, question_id):
+    """Question detail page that can vote the question."""
     question = get_object_or_404(Question, pk=question_id)
     if not question.can_vote():
         messages.error(request, 'Time to vote on questions has expired!')
@@ -36,10 +41,14 @@ def detail(request, question_id):
 
 
 class ResultsView(generic.DetailView):
+    """Question results page that display the score vote of the question."""
+
     model = Question
     template_name = 'polls/results.html'
 
+
 def vote(request, question_id):
+    """Vote function that increase a value of vote and save to vote result."""
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
@@ -55,5 +64,6 @@ def vote(request, question_id):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        return HttpResponseRedirect(reverse('polls:results',
+                                            args=(question.id,)))
 
